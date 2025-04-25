5499a8838601997f1a66d03125f49faf72b752ee
Kanisorn Puttaruksa <kanisornputta@gmail.com>
2025-04-26 01:08:39 +0700

    fix missing 2nd char

--- src/app/components/Typewriter.tsx
+++ src/app/components/Typewriter.tsx
@@ -2,26 +2,25 @@ import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
 const useTypewriter = (text: string, speed = 50) => {
-  const [displayText, setDisplayText] = useState("");
-
-  useEffect(() => {
-    let i = 0;
-    const typingInterval = setInterval(() => {
-      if (i < text.length) {
-        setDisplayText((prevText) => prevText + text.charAt(i));
-        i++;
-      } else {
-        clearInterval(typingInterval);
-      }
-    }, speed);
-
-    return () => {
-      clearInterval(typingInterval);
-    };
-  }, [text, speed]);
-
-  return displayText;
-};
+    const [displayText, setDisplayText] = useState("");
+  
+    useEffect(() => {
+      let i = 0;
+      const type = () => {
+        if (i <= text.length) {
+          setDisplayText(text.slice(0, i));
+          i++;
+          setTimeout(type, speed);
+        }
+      };
+      type();
+  
+      return () => {};
+    }, [text, speed]);
+  
+    return displayText;
+  };
+  
 
 interface TypewriterProps {
   text: string;
